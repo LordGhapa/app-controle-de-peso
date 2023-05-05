064b98f25419dd2e3d5baede987ce280a8535e26
Lord Ghapa <felipef32@hotmail.com>
2023-05-05 20:53:43 -0300

    change div for form

--- src/script.js
+++ src/script.js
@@ -1,28 +1,4 @@
-let lineChart
-/* let weights = {
-  weights: [
-    { day: '2023-04-01', weight: 150 },
-    { day: '2023-04-02', weight: 148 },
-    { day: '2023-04-03', weight: 149 },
-    { day: '2023-04-04', weight: 147 },
-    { day: '2023-04-05', weight: 146 },
-    { day: '2023-04-06', weight: 145 },
-    { day: '2023-04-07', weight: 144 },
-    { day: '2023-04-08', weight: 142 },
-    { day: '2023-04-09', weight: 141 },
-    { day: '2023-04-10', weight: 140 },
-    { day: '2023-04-11', weight: 138 },
-    { day: '2023-04-12', weight: 137 },
-    { day: '2023-04-13', weight: 136 },
-    { day: '2023-04-14', weight: 135 },
-    { day: '2023-04-15', weight: 134 },
-    { day: '2023-04-16', weight: 132 },
-    { day: '2023-04-17', weight: 131 },
-    { day: '2023-04-18', weight: 130 },
-    { day: '2023-04-19', weight: 129 },
-    { day: '2023-04-20', weight: 128 }
-  ]
-} */
+let lineChartfi
 let weights
 
 const blocker = document.querySelector('#blocker')
